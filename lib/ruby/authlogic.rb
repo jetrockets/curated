a4342f8bdb7c8208862c64a22fc1cb84c2943a7d
Igor Alexandrov <igor.alexandrov@gmail.com>
2023-02-10 12:08:48 +0400

    binarylogic/authlogic Ruby gem

---
+++ lib/ruby/authlogic.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Authlogic < Curated::RubyGem
+  package 'authlogic'
+  homepage 'https://github.com/binarylogic/authlogic'
+  category Category::Authentication
+
+  pros "We've been usi Authlogic for since 2010 and always had a great experience with it"
+  pros 'It is easy to use and provides a simple, clean API for handling authentication.'
+  pros 'It is flexible and customizable, allowing developers to modify the behavior of the authentication process to meet their specific needs.'
+  pros 'It is well documented and has an active community of developers contributing to its development, making it a reliable and well-supported solution for authentication.'
+
+  cons 'There is no built-in support for 2FA (two-factor authentication).'
+  cons 'You need two implement all authentication logic yourself. Requires a lot of boilerplate code.'
+end
