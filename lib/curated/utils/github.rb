52e224764fa43698b3026a60a38e123015ba7b3b
Igor Alexandrov <igor.alexandrov@gmail.com>
2023-02-14 14:04:11 +0400

    Updated GitHub API to use GITHUB_TOKEN

--- lib/curated/utils/github.rb
+++ lib/curated/utils/github.rb
@@ -9,10 +9,22 @@ module Utils
   def github_contributors_for_file(repo, path)
     uri = URI("https://api.github.com/repos/#{repo}/commits?path=#{path}&per_page=20")
 
-    response = Net::HTTP.get(uri)
-    commits = JSON.parse(response)
+    request = Net::HTTP::Get.new(uri)
+    if ENV['GITHUB_TOKEN']
+      request["Authorization"] = "Bearer #{ENV['GITHUB_TOKEN']}"
+      request["Accept"] = "application/vnd.github+json"
+    end
 
-    commits.map { |commit| commit['author']['login'] }.uniq
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    if response.code == "200"
+      commits = JSON.parse(response.body)
+      commits.map { |commit| commit['author']['login'] }.uniq
+    else
+      raise "GitHub API error: #{response.code} #{response.body}"
+    end
   end
 
   def github_link_to_login(login)
