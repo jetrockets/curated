4490a82ae52992c5d1da062160f85ead161ccefe
Igor Alexandrov <igor.alexandrov@gmail.com>
2023-02-14 14:06:26 +0400

    Fixed Rubocop offenses

--- lib/curated/utils/github.rb
+++ lib/curated/utils/github.rb
@@ -11,15 +11,16 @@ module Utils
 
     request = Net::HTTP::Get.new(uri)
     if ENV['GITHUB_TOKEN']
-      request["Authorization"] = "Bearer #{ENV['GITHUB_TOKEN']}"
-      request["Accept"] = "application/vnd.github+json"
+      request['Authorization'] = "Bearer #{ENV["GITHUB_TOKEN"]}"
+      request['Accept'] = 'application/vnd.github+json'
     end
 
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-    end
+    response =
+      Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+      end
 
-    if response.code == "200"
+    if response.code == '200'
       commits = JSON.parse(response.body)
       commits.map { |commit| commit['author']['login'] }.uniq
     else
